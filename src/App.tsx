2c45003faa5fed99606e0146f32b305df5a30f27
dballard10 <dylanballard55@gmail.com>
2024-12-30 12:56:55 -0500

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Message.tsx";
+
+function App() {
+  return <Message />;
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+  const title = "Song Editor";
+  return <h1>{title}</h1>;
+}
+
+export default Message;
